052c30f93b29a88bd3b17f19f58c3ae756354e03
fin4le-p <stakarax@gmail.com>
2026-02-26 11:23:15 +0900

    log test

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import os
 import asyncio
+import logging
 
 import discord
 from discord.ext import commands
@@ -10,6 +11,11 @@ from commands.punish import punish_command
 from commands.tactic import tactic_command
 from commands.help import help_command
 from commands.riotcon import riotcon_command
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s %(levelname)s [%(name)s] %(message)s",
+)
 
 load_dotenv()
 DISCORD_TOKEN = os.getenv("DISCORD_TOKEN")
